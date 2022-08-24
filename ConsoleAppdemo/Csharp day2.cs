975c5afd3e4d57b2943bf0dfb2150f7ab7fbf2c7
githubjeevan <105343415+githubjeevan@users.noreply.github.com>
2022-08-24 14:54:52 +0530

    Add files via upload

---
+++ ConsoleAppdemo/Csharp day2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppdemo
+{
+    internal class Csharp_day2function
+    {
+        public static int multiply(int num1,int num2,int num3)
+        {
+            return num1 * num2 * num3 ;
+        }
+
+             public static void Main()
+        {
+            int num1, num2, num3;
+
+            Console.Write("Input the first number to multiply: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Input the second number to multiply: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Input the third number to multiply: ");
+            num3 = int.Parse(Console.ReadLine());
+
+            int result = num1 * num2 * num3;
+            Console.WriteLine($"the multiplication of three numbers are {result}");
+        }
+
+    }
+}
---
+++ ConsoleAppdemo/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Myclass
+{
+    enum Grade { pass = 60, distinction = 85 };
+
+    public static void Main()
+    {
+        int empid;
+        int mark;
+
+        Console.WriteLine("whats your empid?");
+        empid = int.Parse(Console.ReadLine());
+        Console.WriteLine("hello world");
+        Console.WriteLine($"thanks for giving empid {empid}");
+        Console.WriteLine("enter your tsql mark?");
+        mark = int.Parse(Console.ReadLine());
+        if (mark >= (int)Grade.distinction)
+                {
+                     Console.WriteLine("you are in distincation grade");
+                }
+        else if (mark >= (int)Grade.pass)
+       
+            { 
+            Console.WriteLine("you are in pass grade"); 
+            }
+        else
+                 {
+                     Console.WriteLine("sorry, your grade is fail");
+                 }
+    }
+}
